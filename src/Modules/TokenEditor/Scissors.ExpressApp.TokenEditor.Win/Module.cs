a3b46e95ff088bb25dd675e3be31a369d2b67fff
Manuel Grundner <m.grundner@delegate.at>
2017-12-07 16:22:41 +0100

    Added Empty Module

---
+++ src/Modules/TokenEditor/Scissors.ExpressApp.TokenEditor.Win/Module.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Win.SystemModule;
+
+namespace Scissors.TokenEditor.Win
+{
+    public class TokenEditorWindowsFormsModule : ModuleBase
+    {
+        public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB)
+            => ModuleUpdater.EmptyModuleUpdaters;
+
+        protected override IEnumerable<Type> GetDeclaredControllerTypes()
+            => Type.EmptyTypes;
+
+        protected override IEnumerable<Type> GetDeclaredExportedTypes()
+            => Type.EmptyTypes;
+
+        protected override IEnumerable<Type> GetRegularTypes()
+            => Type.EmptyTypes;
+
+        protected override ModuleTypeList GetRequiredModuleTypesCore()
+            => new ModuleTypeList(
+                typeof(SystemModule),
+                typeof(SystemWindowsFormsModule)
+            );
+        
+        protected override void RegisterEditorDescriptors(EditorDescriptorsFactory editorDescriptorsFactory)
+        {
+            base.RegisterEditorDescriptors(editorDescriptorsFactory);
+        }
+    }
+}
