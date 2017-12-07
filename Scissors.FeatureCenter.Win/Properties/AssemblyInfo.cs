2e8d4a0284975c10a5c43894cd61384fbc7f8df6
Manuel Grundner <m.grundner@delegate.at>
2017-12-07 16:56:56 +0100

    Removed Analyziers

--- Scissors.FeatureCenter.Win/Properties/AssemblyInfo.cs
+++ Scissors.FeatureCenter.Win/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -29,4 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.*")]
+[assembly: NeutralResourcesLanguage("en")]
 
