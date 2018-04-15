f4b4b71275c53092872e4f35b5cea29ef495b213
Manuel Grundner <m.grundner@delegate.at>
2018-04-15 11:15:18 +0200

    Added precache for win10 project

--- Scissors.FeatureCenter.Win10/FeatureCenterWindowsFormsApplication.cs
+++ Scissors.FeatureCenter.Win10/FeatureCenterWindowsFormsApplication.cs
@@ -7,6 +7,7 @@ namespace Scissors.FeatureCenter.Win
 {
     public partial class FeatureCenterWindowsFormsApplication : WinApplication
     {
+#if DEBUG
         protected override string GetDcAssemblyFilePath()
             => Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, ApplicationName, DcAssemblyFileName);
 
@@ -18,7 +19,21 @@ namespace Scissors.FeatureCenter.Win
 
         protected override string GetModulesVersionInfoFilePath()
            => Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, ApplicationName, ModulesVersionInfoFileName);
+#else
+        string OutputDirectory => Path.GetDirectoryName(GetType().Assembly.Location);
 
+        protected override string GetDcAssemblyFilePath()
+            => Path.Combine(OutputDirectory, DcAssemblyFileName);
+
+        protected override string GetModelAssemblyFilePath()
+            => Path.Combine(OutputDirectory, ModelAssemblyFileName);
+
+        protected override string GetModelCacheFileLocationPath()
+            => OutputDirectory;
+
+        protected override string GetModulesVersionInfoFilePath()
+           => Path.Combine(OutputDirectory, ModulesVersionInfoFileName);
+#endif
         protected override void OnCustomGetUserModelDifferencesPath(CustomGetUserModelDifferencesPathEventArgs args)
             => args.Path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, ApplicationName);
     }
