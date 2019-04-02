504b73bf83042b022147e1784a301153f5c4f2cf
Manuel Grundner <m.grundner@delegate.at>
2019-04-02 10:54:35 +0200

    feat: upgrade to 18.2.7

--- Scissors.FeatureCenter.Win10/Properties/AssemblyInfo.cs
+++ Scissors.FeatureCenter.Win10/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
-using System.Reflection;
+﻿using System.Reflection;
 
 [assembly: AssemblyVersion("172.0.0.0")]
 [assembly: AssemblyFileVersion("172.0.0.0")]
-[assembly: AssemblyInformationalVersion("172.0.0-topic-Squirrel-OnGithub-6.19+Branch.topic/Squirrel-OnGithub-6.Sha.f3f5ec650547be14eb008666a41343465ce198b0")]
+[assembly: AssemblyInformationalVersion("172.0.0-topic-dx-18-2-7.21+Branch.topic/dx-18-2-7.Sha.bdcb39f3a7e65682ee1212a8b8e318e44a487321")]
