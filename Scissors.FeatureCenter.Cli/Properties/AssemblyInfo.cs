0b0165ac59aea7f18ec5d44f5498cdee373c3d7e
Manuel Grundner <m.grundner@delegate.at>
2019-04-03 18:51:23 +0200

    build: removed nuget config

--- Scissors.FeatureCenter.Cli/Properties/AssemblyInfo.cs
+++ Scissors.FeatureCenter.Cli/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: AssemblyVersion("172.0.0.0")]
 [assembly: AssemblyFileVersion("172.0.0.0")]
-[assembly: AssemblyInformationalVersion("172.0.0-topic-dx-18-2-7.23+Branch.topic/dx-18-2-7.Sha.f499b38bbe34cd01a7bfbdc9b8a2945030965b1a")]
+[assembly: AssemblyInformationalVersion("172.0.0-topic-dx-18-2-7.26+Branch.topic/dx-18-2-7.Sha.0b262747f119f0f3f7bda731e84db1fe93c1eb78")]
