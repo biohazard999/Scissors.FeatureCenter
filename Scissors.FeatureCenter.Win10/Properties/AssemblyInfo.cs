735bb4af27215e326498fbe984fb9d71074876dd
Manuel Grundner <m.grundner@delegate.at>
2019-04-03 18:25:51 +0200

    build: added nuget.config

--- Scissors.FeatureCenter.Win10/Properties/AssemblyInfo.cs
+++ Scissors.FeatureCenter.Win10/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: AssemblyVersion("172.0.0.0")]
 [assembly: AssemblyFileVersion("172.0.0.0")]
-[assembly: AssemblyInformationalVersion("172.0.0-topic-dx-18-2-7.21+Branch.topic/dx-18-2-7.Sha.bdcb39f3a7e65682ee1212a8b8e318e44a487321")]
+[assembly: AssemblyInformationalVersion("172.0.0-topic-dx-18-2-7.23+Branch.topic/dx-18-2-7.Sha.f499b38bbe34cd01a7bfbdc9b8a2945030965b1a")]
