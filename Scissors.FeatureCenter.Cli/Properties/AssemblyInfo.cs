1e353f7f1982d1f4fa0c68d06fb54007a6f545fc
Manuel Grundner <m.grundner@delegate.at>
2019-04-05 15:57:12 +0200

    build: added new cert

--- Scissors.FeatureCenter.Cli/Properties/AssemblyInfo.cs
+++ Scissors.FeatureCenter.Cli/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: AssemblyVersion("172.0.0.0")]
 [assembly: AssemblyFileVersion("172.0.0.0")]
-[assembly: AssemblyInformationalVersion("172.0.0-topic-dx-18-2-7.33+Branch.topic/dx-18-2-7.Sha.85b3ff570c85d789227b08fff8a22878dab3ad34")]
+[assembly: AssemblyInformationalVersion("172.0.0-topic-dx-18-2-7.35+Branch.topic/dx-18-2-7.Sha.190b8f17490d9c1b2713bb931378fa55570cc515")]
