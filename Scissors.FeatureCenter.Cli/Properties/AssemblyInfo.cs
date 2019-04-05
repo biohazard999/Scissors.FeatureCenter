3569c46c9a643818e94085f43201492437cea2cb
Manuel Grundner <m.grundner@delegate.at>
2019-04-05 10:30:38 +0200

    chore: versions bumped

--- Scissors.FeatureCenter.Cli/Properties/AssemblyInfo.cs
+++ Scissors.FeatureCenter.Cli/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: AssemblyVersion("172.0.0.0")]
 [assembly: AssemblyFileVersion("172.0.0.0")]
-[assembly: AssemblyInformationalVersion("172.0.0-topic-dx-18-2-7.26+Branch.topic/dx-18-2-7.Sha.0b262747f119f0f3f7bda731e84db1fe93c1eb78")]
+[assembly: AssemblyInformationalVersion("172.0.0-topic-dx-18-2-7.29+Branch.topic/dx-18-2-7.Sha.5690a49d3a76dc1fd45a10c1be5faab880133556")]
