2ed886d19600c6e1e710b84f8175d10105a54439
Manuel Grundner <m.grundner@delegate.at>
2019-04-07 13:07:32 +0200

    feat: added snipplets

--- Scissors.FeatureCenter.Win10/Properties/AssemblyInfo.cs
+++ Scissors.FeatureCenter.Win10/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: AssemblyVersion("172.0.0.0")]
 [assembly: AssemblyFileVersion("172.0.0.0")]
-[assembly: AssemblyInformationalVersion("172.0.0-topic-use-generators.41+Branch.topic/use-generators.Sha.05847d38a528be417512cfeb9d97f04023d742a7")]
+[assembly: AssemblyInformationalVersion("172.0.0-topic-new-theme.43+Branch.topic/new-theme.Sha.882441f883a2ff6c236a2b465517cfe6ab5f8788")]
