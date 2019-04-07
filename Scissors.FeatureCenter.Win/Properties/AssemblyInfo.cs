d1b4f80aba1a298f5e1064dd3f5bdd877f31ee92
Manuel Grundner <m.grundner@delegate.at>
2019-04-07 10:33:57 +0200

    fix: made windows references absolute

--- Scissors.FeatureCenter.Win/Properties/AssemblyInfo.cs
+++ Scissors.FeatureCenter.Win/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: AssemblyVersion("172.0.0.0")]
 [assembly: AssemblyFileVersion("172.0.0.0")]
-[assembly: AssemblyInformationalVersion("172.0.0-topic-dx-18-2-7.35+Branch.topic/dx-18-2-7.Sha.190b8f17490d9c1b2713bb931378fa55570cc515")]
+[assembly: AssemblyInformationalVersion("172.0.0-topic-dx-18-2-7.39+Branch.topic/dx-18-2-7.Sha.ff89fb75abf9847f8398bb60b63529f8f474748b")]
