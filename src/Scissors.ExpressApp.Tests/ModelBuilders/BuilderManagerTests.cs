8dc774721f23979252fb8097c5113589915cde11
Manuel Grundner <m.grundner@delegate.at>
2019-05-09 13:21:34 +0200

    Added XafBuilder

--- src/Scissors.ExpressApp.Tests/ModelBuilders/BuilderManagerTests.cs
+++ src/Scissors.ExpressApp.Tests/ModelBuilders/BuilderManagerTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevExpress.ExpressApp.DC;
 using FakeItEasy;
 using Scissors.ExpressApp.ModelBuilders;
 using Xunit;
@@ -39,6 +40,64 @@ namespace Scissors.ExpressApp.Tests.ModelBuilders
 
                 A.CallTo(() => builderA.Build()).MustHaveHappenedOnceExactly()
                     .Then(A.CallTo(() => builderB.Build()).MustHaveHappenedOnceExactly());
+            }
+        }
+    }
+
+    public class XafBuilderManagerTests
+    {
+        public class AddBuilderBuild
+        {
+            [Fact]
+            public void CallsRefreshAfterBuild()
+            {
+                var typesInfo = A.Fake<ITypesInfo>();
+
+                IBuilderManager sut = new XafBuilderManager(typesInfo);
+                var builderA = A.Fake<IBuilder>(o => o.Implements<ITypeInfoProvider>());
+                var builderB = A.Fake<IBuilder>(o => o.Implements<ITypeInfoProvider>());
+
+                var typeInfoA = A.Fake<ITypeInfo>();
+                var typeInfoB = A.Fake<ITypeInfo>();
+
+                A.CallTo(() => ((ITypeInfoProvider)builderA).TypeInfo).Returns(typeInfoA);
+                A.CallTo(() => ((ITypeInfoProvider)builderB).TypeInfo).Returns(typeInfoB);
+
+                sut
+                    .AddBuilder(builderA)
+                    .AddBuilder(builderB)
+                    .Build();
+
+                A.CallTo(() => builderA.Build()).MustHaveHappenedOnceExactly()
+                    .Then(A.CallTo(() => typesInfo.RefreshInfo(typeInfoA)).MustHaveHappenedOnceExactly())
+                    .Then(A.CallTo(() => builderB.Build()).MustHaveHappenedOnceExactly())
+                    .Then(A.CallTo(() => typesInfo.RefreshInfo(typeInfoB)).MustHaveHappenedOnceExactly());
+            }
+        }
+
+        public class AddBuilders
+        {
+            public class TestXafBuilderManager : XafBuilderManager
+            {
+                public TestXafBuilderManager(ITypesInfo typesInfo) : base(typesInfo) { }
+
+                public IBuilder Builder = A.Fake<IBuilder>();
+                
+                protected override void AddBuilders()
+                {
+                    base.AddBuilders();
+                    AddBuilder(Builder);
+                }
+            }
+
+            [Fact]
+            public void WasCalled()
+            {
+                var sut = new TestXafBuilderManager(A.Fake<ITypesInfo>());
+
+                sut.Build();
+
+                A.CallTo(() => sut.Builder.Build()).MustHaveHappened();
             }
         }
     }
