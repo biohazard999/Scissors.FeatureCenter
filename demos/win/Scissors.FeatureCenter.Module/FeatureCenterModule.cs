2c84a66f4cb9bff71720361869d5f93313344f57
Manuel Grundner <m.grundner@delegate.at>
2019-05-11 06:50:38 +0200

    fix: stackoverflow

--- demos/win/Scissors.FeatureCenter.Module/FeatureCenterModule.cs
+++ demos/win/Scissors.FeatureCenter.Module/FeatureCenterModule.cs
@@ -17,7 +17,6 @@ namespace Scissors.FeatureCenter.Module
         protected override ModuleTypeList GetRequiredModuleTypesCore()
             => base.GetRequiredModuleTypesCore()
                 .AndModuleTypes(
-                    typeof(FeatureCenterModule),
                     typeof(LabelEditorModule)
                 );
 
