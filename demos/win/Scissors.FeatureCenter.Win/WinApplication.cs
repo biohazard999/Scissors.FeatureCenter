a085d0fcf10f7125161526d2c2dae60db598b236
Manuel Grundner <m.grundner@delegate.at>
2019-05-12 10:00:05 +0200

    fix: try to fix module loading

--- demos/win/Scissors.FeatureCenter.Win/WinApplication.cs
+++ demos/win/Scissors.FeatureCenter.Win/WinApplication.cs
@@ -1,4 +1,5 @@
 using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Model.Core;
 using DevExpress.ExpressApp.Utils;
 using DevExpress.ExpressApp.Validation;
 using DevExpress.ExpressApp.Validation.Win;
@@ -6,6 +7,7 @@ using DevExpress.ExpressApp.Win;
 using DevExpress.ExpressApp.Xpo;
 using Scissors.ExpressApp.InlineEditForms.Win;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
@@ -20,15 +22,15 @@ namespace Scissors.FeatureCenter.Win
             LinkNewObjectToParentImmediately = false;
             OptimizedControllersCreation = true;
             UseLightStyle = true;
-            EnableModelCache = false;
+            EnableModelCache = true;
             ExecuteStartupLogicBeforeClosingLogonWindow = true;
         }
-        
+
         static FeatureCenterWindowsFormsApplication()
         {
             DevExpress.ExpressApp.ModelCacheManager.UseMultithreadedLoading = true;
             DevExpress.ExpressApp.ModelCacheManager.SkipEmptyNodes = true;
-            DevExpress.ExpressApp.ModelCacheManager.UseCacheWhenDebuggerIsAttached = false;
+            DevExpress.ExpressApp.ModelCacheManager.UseCacheWhenDebuggerIsAttached = true;
             DevExpress.Persistent.Base.PasswordCryptographer.EnableRfc2898 = true;
             DevExpress.Persistent.Base.PasswordCryptographer.SupportLegacySha512 = false;
             DevExpress.ExpressApp.Utils.ImageLoader.Instance.UseSvgImages = true;
@@ -38,7 +40,23 @@ namespace Scissors.FeatureCenter.Win
         {
             InitializeComponent();
             InitializeDefaults();
+            CreateCustomModelCacheManager += FeatureCenterWindowsFormsApplication_CreateCustomModelCacheManager;
         }
+
+        private void FeatureCenterWindowsFormsApplication_CreateCustomModelCacheManager(object sender, CreateCustomModelCacheManagerEventArgs e)
+        {
+            var p = GetModelCacheFileLocationPath();
+            var cacheFile = Path.Combine(p, ModelStoreBase.ModelCacheDefaultName + ModelStoreBase.ModelFileExtension);
+
+            if(File.Exists(cacheFile))
+            {
+                e.ModelCacheManager = new CustomModelCacheManager(                   
+                    File.Open(cacheFile, FileMode.Open, FileAccess.Read, FileShare.Read),
+                    p
+                );
+            }
+        }
+
 
         protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args)
         {
@@ -69,6 +87,59 @@ namespace Scissors.FeatureCenter.Win
             }
 
             base.LoadUserDifferences();
+        }
+
+        protected override string GetDcAssemblyFilePath()
+            => GetFilePath2("DcAssembly.dll");
+
+        protected override string GetModelAssemblyFilePath()
+            => GetFilePath2("ModelAssembly.dll");
+
+        protected override string GetModulesVersionInfoFilePath()
+            => GetFilePath2("ModulesVersionInfo");
+
+        protected string GetFilePath2(string fileName)
+        {
+            if(!string.IsNullOrEmpty(UserModelDifferenceFilePath))
+            {
+                return Path.Combine(UserModelDifferenceFilePath, fileName);
+            }
+
+            var modelDifferencesPath = GetUserModelDifferencesPath2();
+            if(!string.IsNullOrEmpty(modelDifferencesPath))
+            {
+                return Path.Combine(modelDifferencesPath, fileName);
+            }
+
+            return null;
+        }
+
+        private string GetUserModelDifferencesPath2()
+        {
+            var args = new CustomGetUserModelDifferencesPathEventArgs("")
+            {
+                Path = GetFileLocationPath("UserModelDiffsLocation")
+            };
+            OnCustomGetUserModelDifferencesPath(args);
+            return args.Path;
+        }
+
+    }
+
+    public class CustomModelCacheManager : ModelCacheManager
+    {
+        public CustomModelCacheManager(Stream stream, string modelCacheFileLocationPath) : base(stream, modelCacheFileLocationPath)
+        {
+        }
+
+        protected override void Save(ModelApplicationBase model)
+        {
+            //base.Save(model);
+        }
+
+        protected override void SaveCore(Dictionary<string, string> serializedModel)
+        {
+            //base.SaveCore(serializedModel);
         }
     }
 }
