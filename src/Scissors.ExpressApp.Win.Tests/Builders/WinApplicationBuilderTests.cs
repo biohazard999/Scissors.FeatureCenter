0aef1d082ac05d4b4d0816c1d04345dfc1f23587
Manuel Grundner <m.grundner@delegate.at>
2019-05-17 13:04:10 +0200

    feat: added WinApplicationBuilderTests

---
+++ src/Scissors.ExpressApp.Win.Tests/Builders/WinApplicationBuilderTests.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Win.Core;
+using FakeItEasy;
+using Scissors.ExpressApp.Win.Builders;
+using Shouldly;
+using Xunit;
+
+namespace Scissors.ExpressApp.Win.Tests.Builders
+{
+    public class WinApplicationBuilderTests
+    {
+        public class WithSplashScreen
+        {
+            [Fact]
+            public void HasSplashScreen()
+            {
+                var splash = A.Fake<ISplash>();
+                var application = new WinApplicationBuilder()
+                    .WithSplashScreen(splash)
+                    .Build();
+
+                application.SplashScreen.ShouldBe(splash);
+            }
+
+            [Fact]
+            public void UsesSplashScreen()
+            {
+                var splash = A.Fake<ISplash>();
+                var application = new WinApplicationBuilder()
+                    .WithSplashScreen(splash)
+                    .Build();
+
+                application.StartSplash();
+                application.StopSplash();
+
+                A.CallTo(() => splash.Start()).MustHaveHappened()
+                    .Then(A.CallTo(() => splash.Stop()).MustHaveHappened());
+            }
+        }
+    }
+}
--- src/Scissors.ExpressApp.Win/Builders/WinApplicationBuilder.cs
+++ src/Scissors.ExpressApp.Win/Builders/WinApplicationBuilder.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model;
 using DevExpress.ExpressApp.Win;
 using Scissors.ExpressApp.Builders;
 
 namespace Scissors.ExpressApp.Win.Builders
 {
     /// <summary>
-    /// 
+    ///
     /// </summary>
-    public class WinApplicationBuilder : WinApplicationBuilder<WinApplication, WinApplicationBuilder>
-    {
-
-    }
+    public class WinApplicationBuilder : WinApplicationBuilder<WinApplication, WinApplicationBuilder> { }
 
     /// <summary>
-    /// 
+    ///
     /// </summary>
     /// <typeparam name="TApplication"></typeparam>
     /// <typeparam name="TBuilder"></typeparam>
@@ -30,5 +32,52 @@ namespace Scissors.ExpressApp.Win.Builders
         /// </summary>
         /// <returns>An instace of an WinApplication</returns>
         protected override TApplication Create() => (TApplication)new WinApplication();
+
+        public override TApplication Build()
+        {
+            var application = base.Build();
+
+            application.SplashScreen =
+                SplashScreen
+                ?? application.SplashScreen;
+
+            return application;
+        }
+
+        protected ISplash SplashScreen { get; set; }
+        public TBuilder WithSplashScreen(ISplash splashScreen)
+        {
+            SplashScreen = splashScreen;
+            return This;
+        }
+    }
+
+    /// <summary>
+    /// Manages an Headless Winforms XAF application.
+    /// Useful for testing or service work.
+    /// </summary>
+    public class HeadlessWinApplication : WinApplication
+    {
+        /// <summary>
+        /// <para>Creates an instance of the <see cref="HeadlessXafApplication"/> class.</para>
+        /// </summary>
+        public HeadlessWinApplication() { }
+
+        protected override LayoutManager CreateLayoutManagerCore(bool simple) => null;
+        protected override ListEditor CreateListEditorCore(IModelListView modelListView, CollectionSourceBase collectionSource) => null;
+    }
+
+    public class HeadlessWinApplicationBuilder : HeadlessWinApplicationBuilder<HeadlessWinApplication, HeadlessWinApplicationBuilder> { }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="TApplication"></typeparam>
+    /// <typeparam name="TBuilder"></typeparam>
+    public class HeadlessWinApplicationBuilder<TApplication, TBuilder> : WinApplicationBuilder<TApplication, TBuilder>
+        where TApplication : HeadlessWinApplication
+        where TBuilder : HeadlessWinApplicationBuilder<TApplication, TBuilder>
+    {
+        protected override TApplication Create() => (TApplication)new HeadlessWinApplication();
     }
 }
