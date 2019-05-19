c333abba7156f5747cb3dcf30cabe0312c63e864
Manuel Grundner <m.grundner@delegate.at>
2019-05-19 10:28:25 +0200

    test: HeadlessXafApplicationBuilder

--- src/Scissors.ExpressApp/Builders/XafApplicationBuilder.cs
+++ src/Scissors.ExpressApp/Builders/XafApplicationBuilder.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.DC;
 using Scissors.ExpressApp.Contracts;
